32a9052e0ab4519df9db0146f34f5f942a467dff
Robin Bühler <git+r@obin.ch>
2023-10-10 17:42:16 +0800

    feat: remove and add statuses

--- src/components/StatusesFieldset.js
+++ src/components/StatusesFieldset.js
@@ -5,7 +5,6 @@ const template = await loadMarkup("./StatusesFieldset.html", import.meta.url);
 customElements.define(
   "checkin-statuses-fieldset",
   class extends HTMLElement {
-    rowCounter = 0;
 
     constructor() {
       super();
@@ -16,23 +15,48 @@ customElements.define(
       shadowRoot.appendChild(template.content.cloneNode(true));
 
       const addButton = this.shadowRoot.querySelector("#add-status");
+      addButton.addEventListener("click", () => {
+        const nextId = this.shadowRoot.querySelectorAll("tbody tr").length;
+        this.appendEmptyRow(nextId);
+      });
 
+      const tbody = this.shadowRoot.querySelector("tbody");
+      tbody.addEventListener("click", (e) => {
+        const target = e.target.closest("button");
+        switch (target.dataset.action) {
+          case "remove":
+            this.removeRow(target.dataset.id)
+            break;
+          default:
+            break;
+        };
+      })
     }
 
     setStatuses(statuses) {
       const tbody = this.shadowRoot.querySelector("tbody");
-      tbody.innerHTML = statuses.map((s, i) => this.createRow(i, s.icon, s.title)).join('');
+      tbody.innerHTML = "";
+      tbody.append(...statuses.map((s, i) => this.createRow(i, s.icon, s.title)));
+    }
+
+    removeRow(id) {
+      const row = this.shadowRoot.querySelector(`tbody tr[data-id="${id}"]`);
+      row.remove();
+    }
+
+    appendEmptyRow(id) {
+      const tbody = this.shadowRoot.querySelector("tbody");
+      tbody.append(this.createRow(id, "", ""));
     }
 
     createRow(id, icon, title) {
-      const row = `
-        <tr>
-          <td><input name="statuses[${id}][title]" value="${title}" /></td>
-          <td><input name="statuses[${id}][icon]" value="${icon}" /></td>
-          <td><button class="remove-status" data-status-id="${id}">Remove</button></td>
-        </tr>
+      const row = document.createElement("tr");
+      row.dataset.id = id;
+      row.innerHTML = `
+        <td><input name="statuses[${id}][title]" value="${title}" /></td>
+        <td><input name="statuses[${id}][icon]" value="${icon}" /></td>
+        <td><button data-action="remove" data-id="${id}">Remove</button></td>
       `;
-      this.rowCounter++;
       return row;
     }
   },
